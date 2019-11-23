40a6cff21739a8467740e64b3d92c8636be67f14
avickers <avickers.iom@gmail.com>
2019-11-22 20:20:08 -0600

    New Tiles; Features
    * @lizard12995 provided a Census tract shapefile with truncated metadata;
    * tweaked TileLayers;
    * reintroduced GeoJSON layer;
    * moved db init to proper lifecycle cb.
    
    Co-Authored-By: lizard12995 <lizard12995@users.noreply.github.com>

--- src/components/map-base.js
+++ src/components/map-base.js
@@ -5,23 +5,27 @@ import Papa from 'papaparse'
 import Loki from 'lokijs'
 import LokiIndexedAdapter from 'lokijs/src/loki-indexed-adapter'
 //import supers from '../assets/super.json'
-//import tracts from '../assets/tctracts.json'
+import tracts from '../assets/tctracts.json'
 import csv from '../assets/tc_lrs.csv'
 
 export default class MapBase extends Component {
   constructor() {
     super()
-    const idbAdapter = new LokiIndexedAdapter()
-    this.db = new Loki('metadata.db', {
-      adapter: idbAdapter,
-    	autoload: true,
-    	autoloadCallback : this.dbInit.bind(this)
-    })
+    this.db = null
     this.state = {
       lat: 30.2672,
       lng: -97.7431,
       zoom: 11,
     }
+  }
+
+  componentDidMount() {
+    const idbAdapter = new LokiIndexedAdapter()
+    this.db = new Loki('metadata.db', {
+      adapter: idbAdapter,
+      autoload: true,
+      autoloadCallback : this.dbInit.bind(this)
+    })
   }
 
   dbInit() {
@@ -46,8 +50,8 @@ export default class MapBase extends Component {
     return {
       color: '#1f2021',
       weight: 1,
-      fillOpacity: 0.9,
-      fillColor: 'orange',
+      fillOpacity: Math.random(),
+      fillColor: 'silver',
     }
   }
 
@@ -61,20 +65,22 @@ export default class MapBase extends Component {
     return (
       <Map center={position} zoom={this.state.zoom}>
         <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url='https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
         />
-
+        <TileLayer
+          url='https://stamen-tiles-{s}.a.ssl.fastly.net/toner-hybrid/{z}/{x}/{y}{r}.png'
+          attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>'
+        />
+        <GeoJSON
+          data={tracts.features}
+          style={this.geoJSONStyle}
+          onEachFeature={this.onEachFeature}
+        />
       </Map>
     )
   }
 }
-
-// <GeoJSON
-//   data={supers.features}
-//   style={this.geoJSONStyle}
-//   onEachFeature={this.onEachFeature}
-// />
 
 // Marker template for use with events, etc.
 // <Marker position={position}>
