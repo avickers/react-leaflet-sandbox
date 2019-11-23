581c06a70f3690c396a52b86b45e9da0defa769e
avickers <avickers.iom@gmail.com>
2019-11-22 18:09:23 -0600

    Load and parse Census ROAM data from CSV. Persist and index in LokiJS.

--- src/components/map-base.js
+++ src/components/map-base.js
@@ -1,15 +1,44 @@
 import Leaflet from 'leaflet'
 import React, { Component } from 'react'
 import { Map, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet'
-import supers from '../assets/super.json'
+import Papa from 'papaparse'
+import Loki from 'lokijs'
+import LokiIndexedAdapter from 'lokijs/src/loki-indexed-adapter'
+//import supers from '../assets/super.json'
+//import tracts from '../assets/tctracts.json'
+import csv from '../assets/tc_lrs.csv'
 
 export default class MapBase extends Component {
   constructor() {
     super()
+    const idbAdapter = new LokiIndexedAdapter()
+    this.db = new Loki('metadata.db', {
+      adapter: idbAdapter,
+    	autoload: true,
+    	autoloadCallback : this.dbInit.bind(this)
+    })
     this.state = {
       lat: 30.2672,
       lng: -97.7431,
-      zoom: 13,
+      zoom: 11,
+    }
+  }
+
+  dbInit() {
+    let meta = this.db.getCollection("metadata")
+    if (meta === null) {
+      meta = this.db.addCollection("metadata")
+      Papa.parse(csv, {
+        download: true,
+        header: true,
+        complete: data => {
+          meta.insert(data.data)
+          this.db.saveDatabase()
+        }
+      })
+      console.log(meta)
+    } else {
+      console.log(meta)
     }
   }
 
@@ -35,15 +64,17 @@ export default class MapBase extends Component {
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <GeoJSON
-          data={supers.features}
-          style={this.geoJSONStyle}
-          onEachFeature={this.onEachFeature}
-        />
+
       </Map>
     )
   }
 }
+
+// <GeoJSON
+//   data={supers.features}
+//   style={this.geoJSONStyle}
+//   onEachFeature={this.onEachFeature}
+// />
 
 // Marker template for use with events, etc.
 // <Marker position={position}>
